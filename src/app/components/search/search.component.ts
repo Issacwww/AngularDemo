395bd32cb44dd69cb320b8fe87c6ab595b1e70b6
Ssuchao <siw32@pitt.edu>
2018-12-23 16:32:49 -0500

    changes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,54 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { StorageService } from '../../services/storage.service';
+import {Component, OnInit} from '@angular/core';
+import {StorageService} from '../../services/storage.service';
 
-@Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
-})
+@Component(
+    {selector: 'app-search', templateUrl: './search.component.html', styleUrls: ['./search.component.css']}
+)
 export class SearchComponent implements OnInit {
-  public keywords: string;
-  public history: any[] = new Array();
-  constructor( public storage: StorageService) { 
-  }
+    public keywords: string;
+    public history: any = new Map<string, number>();
+    constructor(public storage: StorageService) {}
 
-  ngOnInit() {
-    const searchList: any = this.storage.get('SearchHistory');
-    if (searchList) { 
-      this.history = searchList;
+    ngOnInit() {
+        const searchList: any = this
+            .storage
+            .get('SearchHistory');
+        if (searchList) {
+            this.history = searchList;
+        }
     }
-  }
-  check() {
-    if (this.history.indexOf(this.keywords) === -1) {
-      return true;
+    sort() {
+      this.history = new Map([...this.history.entries()].sort((a, b) => b[1] - a[1]));
     }
-  }
-  doClickSearch() {
-    if (this.check()) {
-      this.history.push(this.keywords);
-      this.storage.set('SearchHistory', this.history);
-    }
-    this.keywords = '';
-  }
-
-  doKeySearch(event) {
-    if (event.keyCode === 13) {
-      if (this.check()) {
-        this.history.push(this.keywords);
+    doClickSearch() {
+        if (this.history.has(this.keywords)) {
+            this.history[this.keywords] += 1;
+        } else {
+            this.history[this.keywords] = 1;
+        }
+        this.sort();
         this.storage.set('SearchHistory', this.history);
-      }
-      this.keywords = '';
+        this.keywords = '';
     }
-  }
 
-  deletHistory(key) {
-    this.history.splice(key, 1);
-    this.storage.set('SearchHistory', this.history);
-  }
+    doKeySearch(event) {
+      if (event.keyCode === 13) {
+        if (this.history.has(this.keywords)) {
+          this.history[this.keywords] += 1;
+        } else {
+          this.history[this.keywords] = 1;
+        }
+        this.sort();
+        this.storage.set('SearchHistory', this.history);
+        this.keywords = '';
+      }
+    }
 
-  deleteAll() {
-    this.keywords = '';
-    this.history = [];
-    this.storage.remove('SearchHistory');
-  }
+    deletHistory(key) {
+        this.history.splice(key, 1);
+        this.storage.set('SearchHistory', this.history);
+    }
+
+    deleteAll() {
+        this.keywords = '';
+        this.history = new Map();
+        this.storage.remove('SearchHistory');
+    }
 }
